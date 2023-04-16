0899dec1507329dddba7e824cae6a57045175460
Anna <tkachenko.an.an@gmail.com>
2023-04-16 13:02:02 +0200

    validation forms

--- src/js/script.js
+++ src/js/script.js
@@ -50,7 +50,43 @@ $(document).ready(function(){
         });
     });
 
+    // validation
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите не менее {0} символов")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свой email",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
   });
+
+
 
 // https://github.com/ganlanyuan/tiny-slider
 
